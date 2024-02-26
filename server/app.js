1a1726771458683a5ce2a19ff2e1c26369a81eb8
Zephyr Pfeiffer <zephyrpfeiffer@gmail.com>
2024-02-26 12:33:04 -0500

    chore/initialize server

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log('Sever started'));
+
+const items = [
+
+  {
+    name: 'Baba Ultra Laptop',
+    model: 'baba240X',
+  },
+
+  {
+    name: 'Keke Round Kick Laptop',
+    model: 'kekekick540',
+  }
+
+]
+
+app.get('/api/items', (req, res) => {
+
+  res.send(items)
+
+})
