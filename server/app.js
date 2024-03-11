633778ef57300ac0bd645f992b5d38305357ba9b
Zephyr Pfeiffer <zephyrpfeiffer@gmail.com>
2024-03-11 17:43:06 -0400

    remove test code in server file

--- server/app.js
+++ server/app.js
@@ -7,23 +7,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('Sever started'));
 
 app.use(express.static('build'));
-
-const items = [
-
-  {
-    name: 'Baba Ultra Laptop',
-    model: 'baba240X',
-  },
-
-  {
-    name: 'Keke Round Kick Laptop',
-    model: 'kekekick540',
-  }
-
-]
-
-app.get('/api/items', (req, res) => {
-
-  res.send(items)
-
-})
