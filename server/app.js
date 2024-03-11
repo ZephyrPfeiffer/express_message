f738b548363667b944fa4e7a8cfa293b8656864e
Zephyr Pfeiffer <zephyrpfeiffer@gmail.com>
2024-03-11 17:53:53 -0400

    initialize socket.io on server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 
 const app = express();
-
+const server = createServer(app);
+const io = new Server(server);
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log('Sever started'));
-
 app.use(express.static('build'));
+
+io.on('incoming message', (data) => {
+  console.log(data);
+})
+
+app.listen(PORT, () => console.log('Sever started'));
