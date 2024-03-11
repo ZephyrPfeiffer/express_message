ea752194c7747b6263d6735f4bd2f1a0ecdc3d8b
Zephyr Pfeiffer <zephyrpfeiffer@gmail.com>
2024-03-11 18:30:58 -0400

    feat/add communication between frontend and backend via sockets

--- server/app.js
+++ server/app.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+
+const app = express();
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
-const PORT = process.env.PORT || 8080;
-
+app.use(cors());
 app.use(express.static('build'));
 
-io.on('incoming message', (data) => {
-  console.log(data);
+const server = createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 8080;
+
+io.on('connection', (socket) => {
+  
+  socket.on('incoming message', (message) => {
+    console.log(message);
+  })
+
 })
 
-app.listen(PORT, () => console.log('Sever started'));
+server.listen(PORT, () => console.log('Sever started'));
--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
+const socket = io.connect();
 
 function App() {
+
+  const sendMessage = () => {
+    console.log(socket);
+    socket.emit('incoming message', {message: 'hello'});
+  }
+
   return (
     <div>
-
+      <input placeholder="message..." />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   )
 }
