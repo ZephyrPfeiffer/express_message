ee32bbfa9c5b8e52136f9c5f58fe5efd5b4006bc
Zephyr Pfeiffer <zephyrpfeiffer@gmail.com>
2024-05-20 19:21:21 -0400

    feature/add logic to message-overlay component to display interactable words

--- src/components/MessageOverlay.js
+++ src/components/MessageOverlay.js
@@ -1,5 +1,20 @@
 import style from "./MessageOverlay.module.css";
 
-export default function MessageOverlay() {
-  return <div className={style.overlay}></div>;
+export default function MessageOverlay({ message }) {
+  const words = message.split(" ");
+  const interactableOverlayText = [];
+
+  function handleClick() {
+    console.log("hello");
+  }
+
+  for (let i = 0; i < words.length; i++) {
+    interactableOverlayText.push(
+      <span className={style.word} key={i} onClick={handleClick}>
+        {words[i]}
+      </span>
+    );
+  }
+
+  return <div className={style.overlay}>{interactableOverlayText}</div>;
 }
